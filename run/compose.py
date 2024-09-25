e80f92cf2bcfec3107e26581e3bf80099e40fc70
wjq <w@w>
2024-09-26 01:53:22 +0200

    count bbls

--- run/compose.py
+++ run/compose.py
@@ -2,10 +2,36 @@
 import sys
 import subprocess
 import json
+import os
+import r2pipe
 
 utk_path = "/home/w/go/bin/utk"
+uefiextract_path = "./uefiextract"
 
+def count_basic_blocks(exe_file):
+    # Open the binary with Radare2 in analysis mode
+    r2 = r2pipe.open(exe_file)
+    r2.cmd('aaa')  # Perform auto analysis
+    
+    # Get all the functions in the binary
+    functions = r2.cmdj('aflj')  # JSON output of all functions
+    if not functions:
+        print("No functions found in the binary.")
+        return 0
 
+    total_basic_blocks = 0
+
+    # Iterate through each function to count its basic blocks
+    for func in functions:
+        # Get basic block information for the function
+        function_addr = func['offset']
+        basic_blocks = r2.cmdj(f'afbj @ {function_addr}')  # Get basic blocks as JSON
+        if basic_blocks:
+            total_basic_blocks += len(basic_blocks)
+
+    print(f"Total Basic Blocks: {total_basic_blocks}")
+    r2.quit()
+    return total_basic_blocks
 
 def find_dicts_with_key(obj,key_):
     results = []
@@ -52,14 +78,26 @@ def extract_smm_modules(input_firmware):
     return modules
 
 def insert_smm_modules(ovmf_firmware,input_firmware,smm_modules):
+    total_bbl = 0
     for module in smm_modules:
         print(module)
+
         utk_extract_command = [utk_path,input_firmware,"dump",module,"/tmp/smm.ffs"]
         subprocess.run(utk_extract_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
-        utk_insert_command = [utk_path,ovmf_firmware,"insert_after","TcgMorLockSmm","/tmp/smm.ffs","save",ovmf_firmware]
-        subprocess.run(utk_insert_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
+
+        efi_path_name = "/tmp/smm/" + module
+        uefiextract_command = [uefiextract_path,input_firmware,module,"-o",efi_path_name,"-m","body","-t","0x10"]
+        subprocess.run(uefiextract_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
+
+        efi_file_name = efi_path_name + "/body.bin"
+        total_bbl += count_basic_blocks(efi_file_name)
+
+        # utk_insert_command = [utk_path,ovmf_firmware,"insert_after","TcgMorLockSmm","/tmp/smm.ffs","save",ovmf_firmware]
+        # subprocess.run(utk_insert_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
+    print("total bbls {}".format(total_bbl))
 
 
-
+if not os.path.exists("/tmp/smm"):
+    os.mkdir("/tmp/smm")
 smm_modules = extract_smm_modules(sys.argv[1])
 insert_smm_modules(sys.argv[2],sys.argv[1],smm_modules)
