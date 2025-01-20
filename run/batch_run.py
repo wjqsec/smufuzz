2c64806789c63c6f7af6556d47f6867465b18dba
wjq <w@w>
2025-01-20 22:59:37 +0100

    faster uefiextract

--- run/batch_run.py
+++ run/batch_run.py
@@ -11,20 +11,20 @@ ovmf_bin = "/home/w/hd/uefi_fuzz/fuzzer/edk2/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_
 #alien_r3  alien_x51  asus_p453  asus_un65u  game_x570  game_z690  hp_20  hp_obelisk  hp_z2  hp_z440  think_m700  think_p900  think_s30  think_x1
 
 smm_fuzz_projs = [
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_r3/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_r3/Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_x51/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_x51/Alienware X51 R3-dell_alienware_x51_r3"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_p453/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_p453/ASUS P453UJ-P453UJAS.311"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_un65u/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_un65u/ASUS UN65U-UN65U-ASUS-0616.CAP"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_x570/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_x570/X570 GAMING X-X570GX.36e"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_z690/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_z690/Z690 GAMING X-Z690GAMINGX.F3"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_20/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_20/HP 20-c000-hp-20-c000_versopm.bin"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_r3/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_r3/Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_x51/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_x51/Alienware X51 R3-dell_alienware_x51_r3"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_p453/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_p453/ASUS P453UJ-P453UJAS.311"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_un65u/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_un65u/ASUS UN65U-UN65U-ASUS-0616.CAP"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_x570/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_x570/X570 GAMING X-X570GX.36e"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_z690/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_z690/Z690 GAMING X-Z690GAMINGX.F3"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_20/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_20/HP 20-c000-hp-20-c000_versopm.bin"],
 ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_obelisk/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_obelisk/HP Obelisk 875-0821D.bin"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z2/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z2/HP Z2 Mini G4 -31A298"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z440/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z440/HP Z440-M60_0256.bin"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_m700/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_m700/ThinkCentre M700-imagefw.rom"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_p900/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_p900/Thinkstation P900-thinkpadp900.ROM"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_s30/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_s30/ThinkStation S30-IMAGEA2.bios"],
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_x1/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_x1/Thinkpad X1 Fold-x1fold_version.FL1"]
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z2/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z2/HP Z2 Mini G4 -31A298"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z440/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z440/HP Z440-M60_0256.bin"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_m700/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_m700/ThinkCentre M700-imagefw.rom"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_p900/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_p900/Thinkstation P900-thinkpadp900.ROM"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_s30/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_s30/ThinkStation S30-IMAGEA2.bios"],
+["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_x1/","/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_x1/Thinkpad X1 Fold-x1fold_version.FL1"]
 ]
 
 for smm_fuzz_proj in smm_fuzz_projs:
@@ -35,7 +35,7 @@ for smm_fuzz_proj in smm_fuzz_projs:
 wait_f = []
 for smm_fuzz_dir in smm_fuzz_projs: 
     f = open(smm_fuzz_dir[0] + "fuzzer.log", "w")
-    fuzz_command = [fuzz_bin, "--proj",smm_fuzz_dir[0],"fuzz","--fuzz-time","10s" ]
+    fuzz_command = [fuzz_bin, "--proj",smm_fuzz_dir[0],"fuzz","--fuzz-time","10h" ]
     env_vars = os.environ.copy()
     env_vars["RUST_LOG"] = "info"
     result = subprocess.Popen(fuzz_command, stdout=f, stderr=f,env=env_vars )
--- run/compose.py
+++ run/compose.py
@@ -4,8 +4,9 @@ import subprocess
 import json
 import shutil
 import os
+import glob
 import r2pipe
-
+import shutil
 
 ovmf_guids = [
      "A47EE2D8-F60E-42FD-8E58-7BD65EE4C29B",
@@ -32,84 +33,25 @@ use_ovmf_guids = [
    'A3FF0EF5-0C28-42F5-B544-8C7DE1E80014', #dexsmm
    '470CB248-E8AC-473C-BB4F-81069A1FE6FD', #faulttolerantwriteSmm
 #    '2D59F041-53A4-40D0-A6CD-844DC0DFEF17'  #s3smm save state  try to access sram while outside sram code
- ]
+]
 
 
-
-utk_path = "/home/w/go/bin/utk"
+utk_path = "./utk"
 uefiextract_path = "./uefiextract"
-ghidra_analyzeheadless_path = "./ghidra_11.1.2_PUBLIC/support/analyzeHeadless"
-
-ghidra_project_path = "/tmp/ghidra_project"
-
-count_bbl_file_path = "/tmp/count_bbl.py"
-count_bbl_src = '''
-from ghidra.program.model.block import SimpleBlockIterator
-from ghidra.program.model.block import BasicBlockModel
-from ghidra.program.model.block import SimpleBlockModel
-from ghidra.util.task import TaskMonitor
-
-args = getScriptArgs()
-bbm = SimpleBlockModel(currentProgram)
-blocks = bbm.getCodeBlocks(TaskMonitor.DUMMY)
-block = blocks.next()
-bbs = set()
-while block:
-    bbs.add(block.getFirstStartAddress())
-    block = blocks.next()
-print("all basic blocks:{}".format(len(bbs)))
-'''
-
-def count_basic_blocks(exe_file):
-    if os.path.exists(ghidra_project_path):
-        shutil.rmtree(ghidra_project_path)
-    os.mkdir(ghidra_project_path)
-    ghidra_command = [ghidra_analyzeheadless_path, ghidra_project_path,"body.bin","-import", exe_file, "-postScript", count_bbl_file_path ]
-    result = subprocess.run(ghidra_command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
-    if "all basic blocks:" in result.stdout:
-        return int(result.stdout.split("all basic blocks:")[1].split("\n")[0])
-    return 0
-
-def find_dicts_with_key(obj,key_):
-    results = []
-    
-    if isinstance(obj, dict):
-        # Recursively check all values in the dictionary
-        for key, value in obj.items():
-            if key == key_:
-                results.extend(value)
-
-            results.extend(find_dicts_with_key(value,key_))
-    
-    elif isinstance(obj, list):
-        # Recursively check all items in the list
-        for item in obj:
-            results.extend(find_dicts_with_key(item,key_))
-    
-    return results
-
-
-def is_smm_module(module_desc):
-    if isinstance(module_desc, dict):
-        if "Type" in module_desc:
-            # return module_desc["Type"] == "EFI_FV_FILETYPE_MM_CORE"
-            return module_desc["Type"] == "EFI_FV_FILETYPE_MM" or module_desc["Type"] == "EFI_FV_FILETYPE_MM_CORE"
-    return False
 
 def get_all_smm_modules(firmware):
-    utk_command = [utk_path,firmware,"json"]
+    ret = []
+    firmware_filename = os.path.basename(firmware)
+    firmware_filepath = os.path.dirname(firmware)
+    extract_command = [uefiextract_path,firmware,"all"]
 
-    result = subprocess.run(utk_command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, text=True)
+    result = subprocess.run(extract_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
+    with open(firmware + ".report.txt") as f:
+        for line in f.readlines():
+            if "SMM module" in line or "SMM core" in line:
+                ret.append(line.split("|")[-2].split(" ")[-2])
+    return ret
 
-    std_output = result.stdout
-
-    parsed_data = json.loads(std_output)
-
-    found =  find_dicts_with_key(parsed_data,"Files")
-
-    modules = [x["Header"]["GUID"]["GUID"] for x in found if is_smm_module(x)]
-
-    return modules
 
 def delete_smm_module(firmware,modules):
     for module in modules:
@@ -118,46 +60,45 @@ def delete_smm_module(firmware,modules):
         subprocess.run(utk_insert_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
 
 def insert_smm_modules(ovmf_firmware,input_firmware,smm_modules):
-    total_bbl = 0
+    if not os.path.exists("/tmp/smm"):
+        os.mkdir("/tmp/smm")
     last_add = ""
     for module in smm_modules:
         print("add " + module)
-        
-        utk_extract_command = [utk_path,input_firmware,"dump",module,"/tmp/smm.ffs"]
-        subprocess.run(utk_extract_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
+        for folder, subs, files in os.walk(input_firmware + ".dump"):
+            found_info = False
+            for file in files:
+                if os.path.isfile(folder + "info.txt"):
+                    found_info = True
+                    break
+            if not found_info:
+                continue
+            with open(folder + "info.txt") as f:
+                if ("File GUID: " + module) in f.read():
+                    if last_add == "" : 
+                        utk_insert_command = [utk_path,ovmf_firmware,"insert_after","VirtioRngDxe",os.path.join(folder,"body.bin"),"save",ovmf_firmware]
+                    else:
+                        utk_insert_command = [utk_path,ovmf_firmware,"insert_after",last_add,os.path.join(folder,"body.bin"),"save",ovmf_firmware]
+                    last_add = module
+                    subprocess.run(utk_insert_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
 
-        #efi_path_name = "/tmp/smm/" + module
-        #uefiextract_command = [uefiextract_path,input_firmware,module,"-o",efi_path_name,"-m","body","-t","0x10"]
-        #subprocess.run(uefiextract_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
-        
-        #efi_file_name = efi_path_name + "/body.bin"
-        #num_bbl = count_basic_blocks(efi_file_name)
-        #total_bbl += num_bbl
-        #print(num_bbl)
-        if last_add == "" : 
-            utk_insert_command = [utk_path,ovmf_firmware,"insert_after","VirtioRngDxe","/tmp/smm.ffs","save",ovmf_firmware]
-        else:
-            utk_insert_command = [utk_path,ovmf_firmware,"insert_after",last_add,"/tmp/smm.ffs","save",ovmf_firmware]
-        last_add = module
-        
-        subprocess.run(utk_insert_command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, text=True)
-    print("total bbls {}".format(total_bbl))
-def prepare_count_bbl_file():
-    with open(count_bbl_file_path,"w") as f:
-        f.write(count_bbl_src)
+def clean(ovmf_firmware,input_firmware):
+    os.remove(ovmf_firmware + ".report.txt")
+    os.remove(ovmf_firmware + ".guids.csv")
+    shutil.rmtree(ovmf_firmware + ".dump")
+
+    os.remove(input_firmware + ".report.txt")
+    os.remove(input_firmware + ".guids.csv")
+    shutil.rmtree(input_firmware + ".dump")
 
 
-
-if not os.path.exists("/tmp/smm"):
-    os.mkdir("/tmp/smm")
-
-ovmf_path = sys.argv[2]
-vendor_firmware_path = sys.argv[1]
-prepare_count_bbl_file()
-vendor_smm_modules = get_all_smm_modules(vendor_firmware_path)
-ovmf_modules = get_all_smm_modules(ovmf_path)
-ovmf_delete_modules = [x for x in ovmf_modules if x in vendor_smm_modules and x not in use_ovmf_guids]
-delete_smm_module(ovmf_path,ovmf_delete_modules)
-vendor_smm_modules = [x for x in vendor_smm_modules if x not in unsupported_guids and x not in use_ovmf_guids]
-insert_smm_modules(ovmf_path,vendor_firmware_path,vendor_smm_modules)
-
+if __name__ == "__main__":
+    ovmf_path = sys.argv[2]
+    vendor_firmware_path = sys.argv[1]
+    vendor_smm_modules = get_all_smm_modules(vendor_firmware_path)
+    ovmf_modules = get_all_smm_modules(ovmf_path)
+    ovmf_delete_modules = [x for x in ovmf_modules if x in vendor_smm_modules and x not in use_ovmf_guids]
+    delete_smm_module(ovmf_path,ovmf_delete_modules)
+    vendor_smm_modules = [x for x in vendor_smm_modules if x not in unsupported_guids and x not in use_ovmf_guids]
+    insert_smm_modules(ovmf_path,vendor_firmware_path,vendor_smm_modules)
+    clean(vendor_firmware_path, ovmf_path)
