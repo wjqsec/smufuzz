b4b5fd6abc835b5e6c2c81a60bbc93c9e0a4b037
wjq <w@w>
2025-01-22 17:09:04 +0100

    add setup script

---
+++ script/setup.sh
@@ -0,0 +1,2 @@
+apt install build-essential uuid-dev iasl nasm  python-is-python3 git curl libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3.10-env llvm clang vim
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
