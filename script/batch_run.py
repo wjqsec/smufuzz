2586cde56eb2bc5d5ab102cf14e93f44cb64129f
wjq <w@w>
2025-02-28 16:22:33 +0100

    batch run

--- script/batch_run.py
+++ script/batch_run.py
@@ -16,58 +16,60 @@ compose_bin = "./compose.py"
 ovmf_bin = "../edk2/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_CODE.fd"
 ovmf_vars = "../edk2/Build/OvmfX64/RELEASE_GCC5/FV/OVMF_VARS.fd"
 
+prefix = "/home/w/hd/uefi_fuzz/experiments"
 
 smm_fuzz_projs = [
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_r3/","Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/alien_x51/","Alienware X51 R3-dell_alienware_x51_r3"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_p453/","ASUS P453UJ-P453UJAS.311"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/asus_un65u/","ASUS UN65U-UN65U-ASUS-0616.CAP"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_x570/","X570 GAMING X-X570GX.36e"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/game_z690/","Z690 GAMING X-Z690GAMINGX.F3"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_20/","HP 20-c000-hp-20-c000_versopm.bin"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_obelisk/","HP Obelisk 875-0821D.bin"],
+    
+[prefix + "/rsfuzzer/alien_r3/","Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
+[prefix + "/rsfuzzer/alien_x51/","Alienware X51 R3-dell_alienware_x51_r3"],
+[prefix + "/rsfuzzer/asus_p453/","ASUS P453UJ-P453UJAS.311"],
+[prefix + "/rsfuzzer/asus_un65u/","ASUS UN65U-UN65U-ASUS-0616.CAP"],
+[prefix + "/rsfuzzer/game_x570/","X570 GAMING X-X570GX.36e"],
+[prefix + "/rsfuzzer/game_z690/","Z690 GAMING X-Z690GAMINGX.F3"],
+[prefix + "/rsfuzzer/hp_20/","HP 20-c000-hp-20-c000_versopm.bin"],
+[prefix + "/rsfuzzer/hp_obelisk/","HP Obelisk 875-0821D.bin"],
 
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z2/","HP Z2 Mini G4 -31A298"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/hp_z440/","HP Z440-M60_0256.bin"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_m700/","ThinkCentre M700-imagefw.rom"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_p900/","Thinkstation P900-thinkpadp900.ROM"],
-["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_x1/","Thinkpad X1 Fold-x1fold_version.FL1"],
-
-
-["/home/w/hd/uefi_fuzz/experiments/exp/acer_aspirea351/","acer_aspirea351.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/acer_aspirer5371t/","acer_aspirer5371t.fd"],
-["/home/w/hd/uefi_fuzz/experiments/exp/asus_a407ub/","asus_a407ub.rom"],
-["/home/w/hd/uefi_fuzz/experiments/exp/asus_laptop_15_k509fa/","asus_laptop_15_k509fa.rom"],
-["/home/w/hd/uefi_fuzz/experiments/exp/asus_x509da/","asus_x509da.rom"],
-["/home/w/hd/uefi_fuzz/experiments/exp/dell_ispiron145410/","dell_ispiron145410.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/dell_latitude7330/","dell_latitude7330.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/dell_vostro7620/","dell_vostro7620.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/dell_xps1595752in1/","dell_xps1595752in1.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/dell_xps179700/","dell_xps179700.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/gigabyte_aero15oled/","gigabyte_aero15oled.rom"],
-["/home/w/hd/uefi_fuzz/experiments/exp/gigabyte_x3plusr7/","gigabyte_x3plusr7.A0F"],
-["/home/w/hd/uefi_fuzz/experiments/exp/gigabyte_x9dt/","gigabyte_x9dt.B03"],
-
-["/home/w/hd/uefi_fuzz/experiments/exp/hp_8750000/","hp_8750000.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/hp_a1yl6ua/","hp_a1yl6ua.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/lenovo_ideapad_14alc7/","lenovo_ideapad_14alc7.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/lenovo_thinkpadx1tablet1gen/","lenovo_thinkpadx1tablet1gen.FL1"],
-["/home/w/hd/uefi_fuzz/experiments/exp/lenovo_x12in1gen9/","lenovo_x12in1gen9.FL1"],
-["/home/w/hd/uefi_fuzz/experiments/exp/lenovo_x1extreme1gen/","lenovo_x1extreme1gen.FL1"],
-["/home/w/hd/uefi_fuzz/experiments/exp/razer_rz090196x/","razer_rz090196x.bin"],
-["/home/w/hd/uefi_fuzz/experiments/exp/razer_rz0903102/","razer_rz0903102.bin"],
+[prefix + "/rsfuzzer/hp_z2/","HP Z2 Mini G4 -31A298"],
+[prefix + "/rsfuzzer/hp_z440/","HP Z440-M60_0256.bin"],
+[prefix + "/rsfuzzer/think_m700/","ThinkCentre M700-imagefw.rom"],
+[prefix + "/rsfuzzer/think_p900/","Thinkstation P900-thinkpadp900.ROM"],
+[prefix + "/rsfuzzer/think_x1/","Thinkpad X1 Fold-x1fold_version.FL1"],
 
 
-["/home/w/hd/uefi_fuzz/experiments/exp/microsoft_surface_go_wifi/","microsoft_surface_go_wifi.bin"],
+[prefix + "/exp/acer_aspirea351/","acer_aspirea351.bin"],
+[prefix + "/exp/acer_aspirer5371t/","acer_aspirer5371t.fd"],
+[prefix + "/exp/asus_a407ub/","asus_a407ub.rom"],
+[prefix + "/exp/asus_laptop_15_k509fa/","asus_laptop_15_k509fa.rom"],
+[prefix + "/exp/asus_x509da/","asus_x509da.rom"],
+[prefix + "/exp/dell_ispiron145410/","dell_ispiron145410.bin"],
+[prefix + "/exp/dell_latitude7330/","dell_latitude7330.bin"],
+[prefix + "/exp/dell_vostro7620/","dell_vostro7620.bin"],
+[prefix + "/exp/dell_xps1595752in1/","dell_xps1595752in1.bin"],
+[prefix + "/exp/dell_xps179700/","dell_xps179700.bin"],
+[prefix + "/exp/gigabyte_aero15oled/","gigabyte_aero15oled.rom"],
+[prefix + "/exp/gigabyte_x3plusr7/","gigabyte_x3plusr7.A0F"],
+[prefix + "/exp/gigabyte_x9dt/","gigabyte_x9dt.B03"],
 
-["/home/w/hd/uefi_fuzz/experiments/exp/msi_E15G3IMS/","msi_E15G3IMS.107"],
-["/home/w/hd/uefi_fuzz/experiments/exp/msi_E1585IMS/","msi_E1585IMS.318"],
-["/home/w/hd/uefi_fuzz/experiments/exp/msi_E15F4IBA/","msi_E15F4IBA.109"],
+[prefix + "/exp/hp_8750000/","hp_8750000.bin"],
+[prefix + "/exp/hp_a1yl6ua/","hp_a1yl6ua.bin"],
+[prefix + "/exp/lenovo_ideapad_14alc7/","lenovo_ideapad_14alc7.bin"],
+[prefix + "/exp/lenovo_thinkpadx1tablet1gen/","lenovo_thinkpadx1tablet1gen.FL1"],
+[prefix + "/exp/lenovo_x12in1gen9/","lenovo_x12in1gen9.FL1"],
+[prefix + "/exp/lenovo_x1extreme1gen/","lenovo_x1extreme1gen.FL1"],
+[prefix + "/exp/razer_rz090196x/","razer_rz090196x.bin"],
+[prefix + "/exp/razer_rz0903102/","razer_rz0903102.bin"],
 
 
-# ["/home/w/hd/uefi_fuzz/experiments/rsfuzzer/think_s30/","ThinkStation S30-IMAGEA2.bios"],  #broken
-# ["/home/w/hd/uefi_fuzz/experiments/exp/hp_866c6ea/","hp_866c6ea.bin"],   #286 modules
-# ["/home/w/hd/uefi_fuzz/experiments/exp/microsoft_surfacepro_10_for_business/","microsoft_surfacepro_10_for_business.bin"], # no smm modules
+[prefix + "/exp/microsoft_surface_go_wifi/","microsoft_surface_go_wifi.bin"],
+
+[prefix + "/exp/msi_E15G3IMS/","msi_E15G3IMS.107"],
+[prefix + "/exp/msi_E1585IMS/","msi_E1585IMS.318"],
+[prefix + "/exp/msi_E15F4IBA/","msi_E15F4IBA.109"],
+
+
+# [prefix + "/rsfuzzer/think_s30/","ThinkStation S30-IMAGEA2.bios"],  #broken
+# [prefix + "/exp/hp_866c6ea/","hp_866c6ea.bin"],   #286 modules
+# [prefix + "/exp/microsoft_surfacepro_10_for_business/","microsoft_surfacepro_10_for_business.bin"], # no smm modules
 
 ]
 wait_f = []
