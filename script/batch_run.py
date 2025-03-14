a0093a6515120a588324d247346ea7f155810bec
wjq <w@w>
2025-03-14 16:15:43 +0100

    add stream length

--- script/batch_run.py
+++ script/batch_run.py
@@ -20,6 +20,7 @@ ovmf_vars = "../edk2/Build/OvmfX64/RELEASE_GCC/FV/OVMF_VARS.fd"
 prefix = "/home/w/sd/smm_fuzz/exp2"
 fuzz_run_time = "3h"
 fuzz_runs = 1
+save_tmp_snapshot = "--save-tmp-snapshot"
 
 smm_fuzz_projs1 = [
 [prefix + "/rsfuzzer/alien_r3/","Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
@@ -69,11 +70,15 @@ smm_fuzz_projs2 = [
 [prefix + "/exp/razer_rz0903102/","razer_rz0903102.bin"],
 ]
 
+
+smm_fuzz_projs = smm_fuzz_projs1 + smm_fuzz_projs2
+
+
+
+
 running_jobs = []
 waiting_jobs = []
 ctrl_c_pressed = False
-smm_fuzz_projs = smm_fuzz_projs1 + smm_fuzz_projs2
-
 def sigint_handler(signum, frame):
     global waiting_jobs
     global running_jobs
@@ -117,7 +122,7 @@ while True:
         tag = str(smm_fuzz_proj[2])
         os.makedirs(os.path.join(smm_fuzz_proj[0], tag), exist_ok=True)
         f = open(os.path.join(os.path.join(smm_fuzz_proj[0], tag),"fuzzer.log"), "w")
-        fuzz_command = [fuzz_bin, "--proj",smm_fuzz_proj[0], "--tag" , tag, "fuzz","--fuzz-time",fuzz_run_time]
+        fuzz_command = [fuzz_bin, "--proj",smm_fuzz_proj[0], "--tag" , tag, "fuzz","--fuzz-time",fuzz_run_time, save_tmp_snapshot]
         env_vars = os.environ.copy()
         env_vars["RUST_LOG"] = "info"
         result = subprocess.Popen(fuzz_command, stdout=f, stderr=f,env=env_vars)
