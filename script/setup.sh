bf946e6100196132c74865849249c159586ed326
wjqsec <wjq.sec@gmail.com>
2025-04-23 01:26:48 +0200

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,2 +1,2 @@
-apt install build-essential uuid-dev iasl nasm  python-is-python3 git curl libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3.10-env llvm clang vim
+apt install build-essential uuid-dev iasl nasm  python-is-python3 git curl libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3-venv llvm clang vim
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
