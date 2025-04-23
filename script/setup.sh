6ee49e71a134dd4169c3f5b0e80c46702921ad7b
wjqsec <wjq.sec@gmail.com>
2025-04-23 02:30:34 +0200

    Update setup.sh

--- script/setup.sh
+++ script/setup.sh
@@ -1,2 +1,10 @@
-apt install build-essential uuid-dev iasl nasm  python-is-python3 git curl libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3-venv llvm clang vim
+apt install build-essential uuid-dev iasl python-is-python3 git curl libglib2.0-dev libfdt-dev libpixman-1-dev zlib1g-dev ninja-build python3-venv llvm clang vim
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+echo "deb http://archive.ubuntu.com/ubuntu jammy main universe" | tee /etc/apt/sources.list.d/jammy.list
+apt update
+apt install -t jammy nasm
+rm /etc/apt/sources.list.d/jammy.list
+apt update
+apt-get install python3-pip
+pip install psutil
+pip install r2pipe
