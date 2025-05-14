6d29e494fb20492b6a762acb23ffcc27684f5e18
wjq <w@w>
2025-05-14 17:36:54 +0200

    modify cov path

--- script/batch_run.py
+++ script/batch_run.py
@@ -11,9 +11,9 @@ import time
 import threading
 
 #------------------------------------------------------------- config
-prefix = "/root/exp"
-fuzz_run_time = "24h"
-fuzz_runs = 5
+prefix = "/home/w/exp"
+fuzz_run_time = "10s"
+fuzz_runs = 1
 save_tmp_snapshot = True
 
 #-------------------------------------------------------------
@@ -28,20 +28,20 @@ ovmf_vars = "../edk2/Build/OvmfX64/RELEASE_GCC/FV/OVMF_VARS.fd"
 
 smm_fuzz_projs1 = [
 
-[prefix + "/rsfuzzer/alien_r3/","Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
+# [prefix + "/rsfuzzer/alien_r3/","Alienware 13 R3-alienware_13_r3_1.13.0.rom"],
 [prefix + "/rsfuzzer/alien_x51/","Alienware X51 R3-dell_alienware_x51_r3"],
-[prefix + "/rsfuzzer/asus_p453/","ASUS P453UJ-P453UJAS.311"],
-[prefix + "/rsfuzzer/asus_un65u/","ASUS UN65U-UN65U-ASUS-0616.CAP"],
-[prefix + "/rsfuzzer/game_x570/","X570 GAMING X-X570GX.36e"],
-[prefix + "/rsfuzzer/game_z690/","Z690 GAMING X-Z690GAMINGX.F3"], 
-[prefix + "/rsfuzzer/hp_20/","HP 20-c000-hp-20-c000_versopm.bin"],
-[prefix + "/rsfuzzer/hp_obelisk/","HP Obelisk 875-0821D.bin"],
-[prefix + "/rsfuzzer/hp_z2/","HP Z2 Mini G4 -31A298"],
-[prefix + "/rsfuzzer/hp_z440/","HP Z440-M60_0256.bin"],
-[prefix + "/rsfuzzer/think_m700/","ThinkCentre M700-imagefw.rom"],
-[prefix + "/rsfuzzer/think_p900/","Thinkstation P900-thinkpadp900.ROM"],
-[prefix + "/rsfuzzer/think_x1/","Thinkpad X1 Fold-x1fold_version.FL1"],
-[prefix + "/exp/microsoft_surface_go_wifi/","microsoft_surface_go_wifi.bin"],
+# [prefix + "/rsfuzzer/asus_p453/","ASUS P453UJ-P453UJAS.311"],
+# [prefix + "/rsfuzzer/asus_un65u/","ASUS UN65U-UN65U-ASUS-0616.CAP"],
+# [prefix + "/rsfuzzer/game_x570/","X570 GAMING X-X570GX.36e"],
+# [prefix + "/rsfuzzer/game_z690/","Z690 GAMING X-Z690GAMINGX.F3"], 
+# [prefix + "/rsfuzzer/hp_20/","HP 20-c000-hp-20-c000_versopm.bin"],
+# [prefix + "/rsfuzzer/hp_obelisk/","HP Obelisk 875-0821D.bin"],
+# [prefix + "/rsfuzzer/hp_z2/","HP Z2 Mini G4 -31A298"],
+# [prefix + "/rsfuzzer/hp_z440/","HP Z440-M60_0256.bin"],
+# [prefix + "/rsfuzzer/think_m700/","ThinkCentre M700-imagefw.rom"],
+# [prefix + "/rsfuzzer/think_p900/","Thinkstation P900-thinkpadp900.ROM"],
+# [prefix + "/rsfuzzer/think_x1/","Thinkpad X1 Fold-x1fold_version.FL1"],
+# [prefix + "/exp/microsoft_surface_go_wifi/","microsoft_surface_go_wifi.bin"],
 
 
 # [prefix + "/rsfuzzer/think_s30/","ThinkStation S30-IMAGEA2.bios"],  #broken
@@ -58,22 +58,22 @@ smm_fuzz_projs1 = [
 ]
 smm_fuzz_projs2 = [
 
-[prefix + "/exp/razer_rz090196x/","razer_rz090196x.bin"],
-[prefix + "/exp/razer_rz0903102/","razer_rz0903102.bin"],
-[prefix + "/exp/acer_aspirea351/","acer_aspirea351.bin"],
-[prefix + "/exp/acer_aspirer5371t/","acer_aspirer5371t.fd"],
-[prefix + "/exp/asus_a407ub/","asus_a407ub.rom"],
-[prefix + "/exp/asus_laptop_15_k509fa/","asus_laptop_15_k509fa.rom"],
-[prefix + "/exp/asus_x509da/","asus_x509da.rom"],
-[prefix + "/exp/gigabyte_aero15oled/","gigabyte_aero15oled.rom"],
-[prefix + "/exp/gigabyte_x3plusr7/","gigabyte_x3plusr7.A0F"],
-[prefix + "/exp/gigabyte_x9dt/","gigabyte_x9dt.B03"],
-[prefix + "/exp/hp_8750000/","hp_8750000.bin"],
-[prefix + "/exp/lenovo_thinkpadx1tablet1gen/","lenovo_thinkpadx1tablet1gen.FL1"],
-[prefix + "/exp/lenovo_x12in1gen9/","lenovo_x12in1gen9.FL1"],
-[prefix + "/exp/msi_E15G3IMS/","msi_E15G3IMS.107"],
-[prefix + "/exp/msi_E1585IMS/","msi_E1585IMS.318"],
-[prefix + "/exp/msi_E15F4IBA/","msi_E15F4IBA.109"],
+# [prefix + "/exp/razer_rz090196x/","razer_rz090196x.bin"],
+# [prefix + "/exp/razer_rz0903102/","razer_rz0903102.bin"],
+# [prefix + "/exp/acer_aspirea351/","acer_aspirea351.bin"],
+# [prefix + "/exp/acer_aspirer5371t/","acer_aspirer5371t.fd"],
+# [prefix + "/exp/asus_a407ub/","asus_a407ub.rom"],
+# [prefix + "/exp/asus_laptop_15_k509fa/","asus_laptop_15_k509fa.rom"],
+# [prefix + "/exp/asus_x509da/","asus_x509da.rom"],
+# [prefix + "/exp/gigabyte_aero15oled/","gigabyte_aero15oled.rom"],
+# [prefix + "/exp/gigabyte_x3plusr7/","gigabyte_x3plusr7.A0F"],
+# [prefix + "/exp/gigabyte_x9dt/","gigabyte_x9dt.B03"],
+# [prefix + "/exp/hp_8750000/","hp_8750000.bin"],
+# [prefix + "/exp/lenovo_thinkpadx1tablet1gen/","lenovo_thinkpadx1tablet1gen.FL1"],
+# [prefix + "/exp/lenovo_x12in1gen9/","lenovo_x12in1gen9.FL1"],
+# [prefix + "/exp/msi_E15G3IMS/","msi_E15G3IMS.107"],
+# [prefix + "/exp/msi_E1585IMS/","msi_E1585IMS.318"],
+# [prefix + "/exp/msi_E15F4IBA/","msi_E15F4IBA.109"],
 
 ]
 
@@ -120,7 +120,7 @@ while True:
         shutil.copyfile(os.path.join(proj[0], "OVMF_VARS.fd.backup"), os.path.join(proj[0], "OVMF_VARS.fd"))
         os.makedirs(os.path.join(smm_fuzz_proj_dir, tag), exist_ok=True)
         f = open(os.path.join(os.path.join(smm_fuzz_proj_dir, tag),"fuzzer.log"), "w")
-        fuzz_command = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "fuzz","--fuzz-time",fuzz_run_time,"--init-phase-timeout-time","30s"]
+        fuzz_command = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "fuzz","--fuzz-time",fuzz_run_time,"--init-phase-timeout-time","3s"]
         if save_tmp_snapshot:
             fuzz_command.append("--save-tmp-snapshot")
         env_vars = os.environ.copy()
@@ -143,10 +143,10 @@ while True:
                     print("return code {}".format(fd.returncode))
                     print(smm_fuzz_proj)
                 else:
-                    cov_command1 = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "coverage","--cov-module",smm_fuzz_proj_dir+"module.info","--effective-cov-module",smm_fuzz_proj_dir + "effective_module.info","--output",smm_fuzz_proj_dir + "cov1.log"]
+                    cov_command1 = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "coverage","--cov-module",smm_fuzz_proj_dir+"module.info","--effective-cov-module",smm_fuzz_proj_dir + "effective_module.info","--output","cov1.log"]
                     fd = subprocess.Popen(cov_command1, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                     fd.wait()
-                    cov_command2 = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "coverage","--cov-module",smm_fuzz_proj_dir+"effective_module.info", "--include-init-phase","--output",smm_fuzz_proj_dir + "cov2.log"]
+                    cov_command2 = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "coverage","--cov-module",smm_fuzz_proj_dir+"effective_module.info", "--include-init-phase","--output","cov2.log"]
                     fd = subprocess.Popen(cov_command2, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                     fd.wait()
         for f in to_exit:
