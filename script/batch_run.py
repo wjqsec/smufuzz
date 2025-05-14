ec81ce8afafadc8ed68bba839271225464b3f685
wjq <w@w>
2025-05-14 16:03:30 +0200

    add ovmf backup

--- script/batch_run.py
+++ script/batch_run.py
@@ -93,9 +93,9 @@ def sigint_handler(signum, frame):
 
 for proj in smm_fuzz_projs:
     print("Embedding: " + proj[0])
-    shutil.copyfile(ovmf_bin, os.path.join(proj[0], "OVMF_CODE.fd.backdup"))
-    shutil.copyfile(ovmf_vars, os.path.join(proj[0], "OVMF_VARS.fd.backdup"))
-    compose_command = ["python3", compose_bin, os.path.join(proj[0], proj[1]), os.path.join(proj[0], "OVMF_CODE.fd")]
+    shutil.copyfile(ovmf_bin, os.path.join(proj[0], "OVMF_CODE.fd.backup"))
+    shutil.copyfile(ovmf_vars, os.path.join(proj[0], "OVMF_VARS.fd.backup"))
+    compose_command = ["python3", compose_bin, os.path.join(proj[0], proj[1]), os.path.join(proj[0], "OVMF_CODE.fd.backup")]
     result = subprocess.Popen(compose_command)
     running_jobs.append(result)
     for i in range(fuzz_runs):
@@ -116,8 +116,8 @@ while True:
         smm_fuzz_proj_dir = smm_fuzz_proj[0]
         tag = str(smm_fuzz_proj[1])
         avaliable_cpus -= 1
-        shutil.copyfile(os.path.join(proj[0], "OVMF_CODE.fd.backdup"), os.path.join(proj[0], "OVMF_CODE.fd"))
-        shutil.copyfile(os.path.join(proj[0], "OVMF_VARS.fd.backdup"), os.path.join(proj[0], "OVMF_VARS.fd"))
+        shutil.copyfile(os.path.join(proj[0], "OVMF_CODE.fd.backup"), os.path.join(proj[0], "OVMF_CODE.fd"))
+        shutil.copyfile(os.path.join(proj[0], "OVMF_VARS.fd.backup"), os.path.join(proj[0], "OVMF_VARS.fd"))
         os.makedirs(os.path.join(smm_fuzz_proj_dir, tag), exist_ok=True)
         f = open(os.path.join(os.path.join(smm_fuzz_proj_dir, tag),"fuzzer.log"), "w")
         fuzz_command = [fuzz_bin, "--proj",smm_fuzz_proj_dir, "--tag" , tag, "fuzz","--fuzz-time",fuzz_run_time,"--init-phase-timeout-time","30s"]
